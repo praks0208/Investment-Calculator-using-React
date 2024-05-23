fa0bfc008116c2a0222b877a5ac350b42c549ee8
praks0208 <praks.sawant26@gmail.com>
2024-05-23 21:22:57 +0530

    Output Content Conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,11 @@ function App() {
     duration:10
 });
 
+const inputIsValid = userInput.duration>=1;
+
+
+
+
 function handleChange(inputIdentifier, newValue){
   setUserInput(prevUserInput => {
       return{
@@ -25,7 +30,8 @@ function handleChange(inputIdentifier, newValue){
     <>
       <Header />
       <UserInput onChange={handleChange} userInput={userInput}/>
-      <Results input={userInput}/>
+      {!inputIsValid && <p className="center">Please enter a duration greater than 0</p>}
+      {inputIsValid && <Results input={userInput}/>}
     </>
   )
 }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,6 +4,7 @@ export default function Results({ input}){
     const resultsData = calculateInvestmentResults(input);
     const initialInvestment = resultsData[0].valueEndOfYear - resultsData[0].interest - resultsData[0].annualInvestment;
   
+    
 
     return <table id="result">
         <thead>
